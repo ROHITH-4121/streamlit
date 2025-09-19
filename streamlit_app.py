6d002c08882efce1ef10e1b98b3d21b99c355e59
M. Rohith <rohithmerugu78@gmail.com>
2025-09-19 19:05:00 +0530

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,7 @@
+
+import streamlit as st
+
+st.title("Streamlit Practice App")
+st.header("Welcome to Your First Streamlit App")
+st.subheader("Let's Explore Streamlit Features")
+st.write("This app demonstrates various Streamlit components for user inputs, outputs, charts, and file handling.")
