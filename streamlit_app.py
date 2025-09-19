5dd531741cc027dcc9cf47a53ebc407b9e1912ed
M. Rohith <rohithmerugu78@gmail.com>
2025-09-19 19:14:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 
 import streamlit as st
 
-st.title("Streamlit Practice App")
-st.header("Welcome to Your First Streamlit App")
-st.subheader("Let's Explore Streamlit Features")
-st.write("This app demonstrates various Streamlit components for user inputs, outputs, charts, and file handling.")
+st.title("Hello, I am Rohith!")
+st.header("This is my first streamlit app")
+st.subheader("Visualizing data and predictions made easy")
+
+st.write("Welcome to my project! Explore the features and see the predictions.")
